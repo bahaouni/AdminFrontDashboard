e3fc8321d40f3f4ec5dae351f4ec92c123526903
themechanist1 <mlaouhiilyess@gmail.com>
2024-12-15 12:36:17 +0100

    added a lot of stuff

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -16,27 +16,24 @@ export class UsersComponent {
   orders = [
     {
       id: '12345',
-      status: 'Delivered',
-      date: 'December 12, 2023',
-      items: 3,
-      total: 150,
-      badgeClass: 'badge-delivered'
+      firstName: 'ilyess',
+      LastName: 'mlaouhi',
+      Date: 'December 12, 2023',
+      Email: 'mlaouhiilyess@gmail.com'
     },
     {
       id: '12346',
-      status: 'Pending',
-      date: 'December 13, 2023',
-      items: 2,
-      total: 75,
-      badgeClass: 'badge-pending'
+      firstName: 'amine',
+      LastName: 'mlaouhi',
+      Date: 'December 12, 2023',
+      Email: 'mlaouhiilyess@gmail.com'
     },
     {
       id: '12347',
-      status: 'Cancelled',
-      date: 'December 14, 2023',
-      items: 1,
-      total: 45,
-      badgeClass: 'badge-cancelled'
-    }
+      firstName: 'aziz',
+      LastName: 'mlaouhi',
+      Date: 'December 12, 2023',
+      Email: 'mlaouhiilyess@gmail.com'
+    },
   ];
 }
