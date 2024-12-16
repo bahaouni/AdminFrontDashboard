e92925ba3341ea9a0e76d5618a5117d84561ba32
Aziz Dhoukar <aziz.dhoukar@gmail.com>
2024-12-16 12:57:22 +0100

    added client projects page and fixed ports

--- src/app/services/service-a.service.ts
+++ src/app/services/service-a.service.ts
@@ -10,22 +10,22 @@ import {Order} from "../model/Order";
   providedIn: 'root',
 })
 export class ServiceAService {
-  private api = 'http://localhost:8088/api/v1/ServiceA/';
-  private api2 = 'http://localhost:8088/api/v1/Orders/'
-  private api3 = 'http://localhost:8088/api/v1/Users/'
+  private services = 'http://localhost:8089/services/v1/service/';
+  private api2 = 'http://localhost:8088/services/v1/Orders/'
+  private api3 = 'http://localhost:8088/services/v1/Users/'
   constructor(private http: HttpClient) {}
 
   getServices(): Observable<ServiceA[]> {
-    return this.http.get<ServiceA[]>(this.api);
+    return this.http.get<ServiceA[]>(this.services);
   }
   updateService(serviceA: ServiceA): Observable<ServiceA> {
-    return this.http.put<ServiceA>(`${this.api}update`, serviceA);
+    return this.http.put<ServiceA>(`${this.services}/update`, serviceA);
   }
   deleteService(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.api}${id}`);
+    return this.http.delete<void>(`${this.services}${id}`);
   }
   addService(serviceA: ServiceA): Observable<ServiceA> {
-    return this.http.post<ServiceA>(`${this.api}add`, serviceA);
+    return this.http.post<ServiceA>(`${this.services}add`, serviceA);
   }
   getUsers(): Observable<UserDTO[]> {
     return this.http.get<UserDTO[]>(`${this.api3}`);
