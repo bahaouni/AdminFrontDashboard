75187974652deebb010046d9585dc01c1a881dc9
Aziz Dhoukar <aziz.dhoukar@gmail.com>
2024-12-21 19:19:35 +0100

    the front is in the brouser so the env need to be looking from outside the cluster

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  eventURL: 'http://event-management-service:80',
-  orderURL: 'http://order-service:80',
-  authURL: 'http://auth:80',
+  eventURL: 'http://172.18.251.176:30009',
+  orderURL: 'http://172.18.251.176:30003',
+  authURL: 'http://172.18.251.176:30008',
 };
