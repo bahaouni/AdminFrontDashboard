4c656f3bf1419b46ed519b7903d0f539db069789
Aziz Dhoukar <aziz.dhoukar@gmail.com>
2024-12-21 17:09:13 +0100

    added ports to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  eventURL: 'http://event-management-service',
-  orderURL: 'http://order-service',
-  authURL: 'http://auth',
+  eventURL: 'http://event-management-service:30009',
+  orderURL: 'http://order-service:',
+  authURL: 'http://auth:30008',
 };
